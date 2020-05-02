c3068b18b23e794c646bcc32b491e3e7d40033bc
areriff <areriff@users.noreply.github.com>
2020-05-02 08:59:52 +0800

    Update ledABC.py
    added more letters

--- ledABC.py
+++ ledABC.py
@@ -2,10 +2,10 @@ from sense_hat import SenseHat
 from time import sleep
 sense = SenseHat()
 
-X = [100, 0, 0]  # Red
-O = [100, 100, 100]  # White
+X = [255, 0, 0]  # Red
+O = [255, 255, 255]  # White
 
-blankLetter = [
+Letter = [
 O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O,
 O, O, O, O, O, O, O, O,
@@ -49,10 +49,49 @@ O, X, X, O, O, O, O, O,
 O, O, O, X, X, X, X, O,
 ]
 
+LetterD = [
+O, X, X, X, O, O, O, O,
+O, X, O, O, X, X, O, O,
+O, X, O, O, O, O, X, O,
+O, X, O, O, O, O, X, O,
+O, X, O, O, O, O, X, O,
+O, X, O, O, O, O, X, O,
+O, X, O, O, X, X, O, O,
+O, X, X, X, O, O, O, O,
+]
+
+LetterE = [
+O, X, X, X, X, X, X, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, X, X, X, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, X, X, X, X, X, O,
+]
+
+LetterF = [
+O, X, X, X, X, X, X, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, X, X, X, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+O, X, O, O, O, O, O, O,
+]
+
 sense.set_pixels(letterA)
 sleep(0.5)
 sense.set_pixels(LetterB)
 sleep(0.5)
 sense.set_pixels(LetterC)
+sleep(0.5)
+sense.set_pixels(LetterD)
+sleep(0.5)
+sense.set_pixels(LetterE)
+sleep(0.5)
+sense.set_pixels(LetterF)
 sleep(0.5)
 sense.clear()
